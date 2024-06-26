641e2aafeca8b588325340140193c7917311408e
LorisGalleana <loris.galleana@gmail.com>
2024-06-26 21:46:25 +0200

    ESERCIZIO COMPLETATO

--- app.js
+++ app.js
@@ -3,8 +3,40 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: "Hello Vue! Hello World!",
-            image: "https://picsum.photos/id/1074/5000/3333"
+            logoImage: './img/logo.svg',
+            newTask: '',
+            inputError: false,
+            tasks: [
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },{
+                    text: 'Fare la spesa',
+                    done: true
+                },{
+                    text: 'Fare il bucato',
+                    done: false
+                }
+            ]
         }
+    },
+    methods: {
+        //aggiungi task all'inizio
+        addTask() {
+            if (this.newTask !== '' && this.newTask.length >= 5) {
+                this.tasks.unshift(this.newTask);
+                this.inputError = false;
+            }
+            else {
+                this.inputError = true;
+            }
+            //svuoto il valore di newTask
+            this.newTask = '';
+        },
+        //rimuovi task specifico da index
+        removeTask(index) {
+            this.tasks.splice(index, 1);
+        },
+        
     }
-}).mount("#container");
+}).mount("#app");
